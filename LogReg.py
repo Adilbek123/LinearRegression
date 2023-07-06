abca410834546126c8f10b1352e7127c06348c5a
Adilbek123 <adilbek.sultanov93@gmail.com>
2023-07-06 08:29:09 -0600

    added .py file, deleted jupiter

---
+++ LogReg.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, num_iterations=1000, penalty=None):
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.penalty = penalty
+        self.coef_ = None
+        self.intercept_ = None
+
+    
+    def fit(self, X, y, *args, **kwargs):
+        #initialize parameters
+        self.coef_ = np.zeros(X.shape[1])
+        self.intercept_ = 0
+
+        # Perform gradient descent
+        for _ in range(self.num_iterations):
+            # Calculate current predictions
+            linear_model = np.dot(X, self.coef_) + self.intercept_
+            y_pred = 1/(1+np.exp(-linear_model))
+
+            # Calculate gradients
+            dw = (1 / len(X)) * np.dot(X.T, (y_pred - y))
+            db = (1 / len(X)) * np.sum(y_pred - y)
+
+            # Update parameters
+            self.coef_ -= self.learning_rate * dw
+            self.intercept_ -= self.learning_rate * db    
+            
+    def predict(self, X):
+        # Calculate the raw model output
+        linear_model = np.dot(X, self.coef_) + self.intercept_
+
+        # Apply the sigmoid function
+        y_pred = 1/(1+np.exp(-linear_model))
+
+        # Convert probabilities to class labels
+        y_pred_labels = [1 if elem >= 0.5 else 0 for elem in y_pred]
+
+        return np.array(y_pred_labels)
+    
+    def predict_proba(self, X, *args, **kwargs):
+        # Calculate the raw model output
+        linear_model = np.dot(X, self.coef_) + self.intercept_
+
+        # Apply the sigmoid function
+        y_pred = 1/(1+np.exp(-linear_model))
+
+        return y_pred
