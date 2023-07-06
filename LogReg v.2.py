532a9949de5c004b02f77ffb9832f4d5ac528267
Adilbek123 <adilbek.sultanov93@gmail.com>
2023-07-06 17:26:10 -0600

    new version of the file where I write the function myself

---
+++ LogReg v.2.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    sigmoid
+    """
+    return 1/(1 + np.exp(-x))
+
+
+class LogisticRegression(X, y):
+    """
+    """
+
+    def __init__(self, X, y):
+        """
+
+        """
+        pass
+    def fit(self, X, y, penalty="l1", learning_rate = 0.1, num_iter = 1000):
+        """
+        
+        """
+        pass
+    def predict(self, X, y):
+        """
+        
+        """
+        pass
+    def predictProba(self, X, y):
+        """
+        
+        """
+        pass
+
